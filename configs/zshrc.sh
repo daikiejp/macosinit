49847daed567e8eb113b89eb17dc24408fe79f16
Danny Davila <danny@misaki.tech>
2024-01-11 10:26:06 -0500

    chore: change zshrc comments

--- configs/zshrc.sh
+++ configs/zshrc.sh
@@ -1,10 +1,10 @@
 # Install zshrc into the system.
 
-# Check if there are any files in the SSH folder
+# Check if there are any files in the $HOME folder
 if [ -z "$(ls -A $config_folder/$zshrc_file)" ]; then
     abort "No zshrc file was found in $zshrc_file"
 else
-    # Copy the SSH private key to the system SSH folder
+    # Copy the ZSHRC file to the system
     cp "$config_folder/$zshrc_file" "$HOME" && mv "$HOME/$zshrc_file" "$HOME/.$zshrc_file"
     msg "zshrc from "$config_folder/$zshrc_file" copied to $HOME"
 fi
